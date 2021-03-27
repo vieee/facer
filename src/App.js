28f9ad87e30cc0a20eb9f6a4baa130763fc2d086
Deepak Yadav <dgyadav1782000@gmail.com>
2021-03-27 15:53:32 +0530

    Scrapy Myntra in Output

--- src/App.js
+++ src/App.js
@@ -1,25 +1,89 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useRef } from "react";
+import "./App.css";
+import * as tf from "@tensorflow/tfjs";
+import * as facemesh from "@tensorflow-models/facemesh";
+import Webcam from "react-webcam";
+import { drawMesh } from "./meshUtilities.js";
 
 function App() {
+  const webcamReference = useRef(null);
+  const canvasReference = useRef(null);
+
+  const loadFacemesh = async () => {
+    const network = await facemesh.load({
+      inputResolution: { width: 720, height: 500 },
+      scale: 0.8
+    });
+    setInterval(() => {
+      detectFace(network);
+    }, 100);
+  };
+
+  const detectFace = async (network) => {
+    if (
+      typeof webcamReference.current !== "undefined" &&
+      webcamReference.current !== null &&
+      webcamReference.current.video.readyState === 4
+    ) {
+      // Get Video Properties
+      const video = webcamReference.current.video;
+      const videoWidth = webcamReference.current.video.videoWidth;
+      const videoHeight = webcamReference.current.video.videoHeight;
+
+      // Set video width
+      webcamReference.current.video.width = videoWidth;
+      webcamReference.current.video.height = videoHeight;
+
+      // Set canvas width
+      canvasReference.current.width = videoWidth;
+      canvasReference.current.height = videoHeight;
+
+      // Make Detections
+      const faceEstimate = await network.estimateFaces(video);
+      console.log(faceEstimate);
+
+      //Get canvas context
+      const ctx = canvasReference.current.getContext("2d");
+      drawMesh(faceEstimate, ctx);
+    }
+  };
+
+  loadFacemesh();
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Webcam
+        ref={webcamReference}
+        style={{
+          position: "absolute",
+          marginLeft: "auto",
+          marginRight: "auto",
+          left: 0,
+          right: 0,
+          textAlign: "center",
+          zindex: 9,
+          width: 720,
+          height: 500
+        }}
+      />
+
+      <canvas
+        ref={canvasReference}
+        style={{
+          position: "absolute",
+          marginLeft: "auto",
+          marginRight: "auto",
+          left: 0,
+          right: 0,
+          textAlign: "center",
+          zindex: 9,
+          width: 720,
+          height: 500
+        }}
+      />
     </div>
   );
 }
 
 export default App;
+
